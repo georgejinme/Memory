f003fead1ce678889365b978bf0a20d45000ab8e
gougoumemeda <a516550257@qq.com>
2015-07-22 12:16:18 +0800

    初始化项目并且增加指纹验证

---
+++ Memory/Memory/MoLoginError.swift
@@ -0,0 +1,20 @@
+//
+//  MoLoginError.swift
+//  Memory
+//
+//  Created by 钩钩么么哒 on 15/7/22.
+//  Copyright (c) 2015年 钩钩么么哒. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MoLoginErrorController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func addLogo(){
+        
+    }
+}
