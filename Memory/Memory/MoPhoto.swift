4cdd01c00508a573a2306e54aae12757a43c3b98
gougoumemeda <a516550257@qq.com>
2015-07-28 18:20:13 +0800

    引入realm，创建图片和文章

---
+++ Memory/Memory/MoPhoto.swift
@@ -0,0 +1,14 @@
+//
+//  MoPhoto.swift
+//  Memory
+//
+//  Created by 钩钩么么哒 on 15/7/28.
+//  Copyright (c) 2015年 钩钩么么哒. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MoPhoto: Object{
+    dynamic var photo: UIImage?
+}
---
+++ Memory/Memory/MoText.swift
@@ -0,0 +1,9 @@
+//
+//  MoText.swift
+//  Memory
+//
+//  Created by 钩钩么么哒 on 15/7/28.
+//  Copyright (c) 2015年 钩钩么么哒. All rights reserved.
+//
+
+import Foundation
